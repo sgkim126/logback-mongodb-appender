722b3df06a4e2f841712476b97cc8b8c754d6ec7
Seulgi Kim <dev@seulgi.kim>
2014-12-22 01:36:25 +0900

    Make MongoDB client configurable.
    But currently, it only support single address for mongo client. Make it

--- src/main/scala/logback/mongodb/MongoDBAppender.scala
+++ src/main/scala/logback/mongodb/MongoDBAppender.scala
@@ -4,6 +4,39 @@ import ch.qos.logback.classic.spi.ILoggingEvent
 import ch.qos.logback.core.UnsynchronizedAppenderBase
 
 class MongoDBAppender extends UnsynchronizedAppenderBase[ILoggingEvent]  {
+  private var _db: String = "logback"
+  def setDb(name: String) {
+    _db = name
+    withLock { _ =>
+      _mongo = None
+    }(mongoLock)
+  }
+
+  private var _collection: String = "log"
+  def setCollection(name: String) {
+    _collection = name
+    withLock { _ =>
+      _mongo = None
+    }(mongoLock)
+  }
+
+  private var _address: String = "localhost"
+  def setAddress(address: String) {
+    _address = address
+    withLock { _ =>
+      _mongo = None
+    }(mongoLock)
+  }
+
+  private var _port: Int = 27017
+  def setPort(port: Int) {
+    _port = port
+    withLock { _ =>
+      _mongo = None
+    }(mongoLock)
+  }
+
+
   override def append(event: ILoggingEvent) {
   }
 }
